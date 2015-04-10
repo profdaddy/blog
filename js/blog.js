dea591a7e902d0f911d056fb80966d45a7789c81
profdaddy <macdaddy_b@me.com>
2015-04-10 12:34:19 -0400

    loading from parse
    using templates to render parse data

--- js/blog.js
+++ js/blog.js
@@ -4,11 +4,32 @@ $(function() {
  
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("OMykm4qc3kgdhZO1oqYdRcyk0ZXtlNb2A7R4KaBA", "jI3bhTf8rEuZXTc3ckifmaxzUJQSmvnGWNpE1nYE");
- 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
+    
+    var BlogsView = Parse.View.extend({
+    	template: Handlebars.compile($('#blogs-tpl').html()),
+    	render: function() {
+    		var collection = {blog: this.collection.toJSON() };
+    		this.$el.html(this.template(collection));
+    	}
     });
  
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+    	model: Blog
+    });
+    
+    var blogs = new Blogs();
+    
+    blogs.fetch({
+    	success: function(blogs) {
+    		var blogsView = new BlogsView({ collection: blogs });
+    		blogsView.render();
+    		$('.main-container').html(blogsView.el);
+    	},
+    	error: function(blogs, error) {
+    		console.log(error);
+    	}
+    }); 
+    
+
 });
